f584b22f0bdea79b05cf684c31acd89bdbbdfce3
Medouchene <medoen02@gmail.com>
2023-11-14 17:15:41 +0100

    controle en utilisant les flèches

--- Game/src/main/java/main/KeyHandler.java
+++ Game/src/main/java/main/KeyHandler.java
@@ -20,16 +20,16 @@ public class KeyHandler implements KeyListener{
 
 		int code = e.getKeyCode(); //returns the integer keyCode associated with the key in this event
 		
-		if(code == KeyEvent.VK_Z) {
+		if(code == KeyEvent.VK_Z || code == KeyEvent.VK_UP) {
 			upPressed = true;
 		}
-		if(code == KeyEvent.VK_S) {
+		if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
 			downPressed = true;
 		}
-		if(code == KeyEvent.VK_Q) {
+		if(code == KeyEvent.VK_Q || code == KeyEvent.VK_LEFT) {
 			leftPressed = true;
 		}
-		if(code == KeyEvent.VK_D) {
+		if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
 			rightPressed = true;
 		}
 	}
@@ -39,16 +39,16 @@ public class KeyHandler implements KeyListener{
 		
 		int code = e.getKeyCode();
 		
-		if(code == KeyEvent.VK_Z) {
+		if(code == KeyEvent.VK_Z || code == KeyEvent.VK_UP) {
 			upPressed = false;
 		}
-		if(code == KeyEvent.VK_S) {
+		if(code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
 			downPressed = false;
 		}
-		if(code == KeyEvent.VK_Q) {
+		if(code == KeyEvent.VK_Q || code == KeyEvent.VK_LEFT) {
 			leftPressed = false;
 		}
-		if(code == KeyEvent.VK_D) {
+		if(code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
 			rightPressed = false;
 		}		
 		
